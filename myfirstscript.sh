cb0d854c42e2d1aa4acd260513d857d4a9e153c9
seunsmooth <seunsmooth@yahoo.com>
2019-03-18 10:13:27 +0000

    updated a deployment script

--- myfirstscript.sh
+++ myfirstscript.sh
@@ -1,1 +1,2 @@
 ssh frontend-user@172.31.22.228 -v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null 'touch /tmp/neworld'
+ssh frontend-user@172.31.22.228 -v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null 'touch /tmp/neworld_helloworld4'
